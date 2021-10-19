a311727413d63f7932d1957eadaaa027aeb54cb7
richarquispe-prog <richarrey4@gmail.com>
2021-10-19 11:40:19 -0500

    hola

--- includes/footer.php
+++ includes/footer.php
@@ -1,32 +1,42 @@
-
-<br><br><br><br><br>
+<br><br><br><br><br><br>
 <footer class="pie_pagina">
-<!--    <div class="container-fluid">-->
-<!--        <div class="d-flex align-items-center justify-content-between small">-->
-<!--            <div class="text-muted"> &copy;Equipo de Desarrollo@PRODUCT OWNER: Mg Gilmer Matos Vila@SCRUM MASTER: Alexander Briceño carhuancho--DEVELOPMENT TEAM:@Lanasca Guillermo Karem@Herrera Paucar Yelina@Huamán Campos Roberth@Chamorro Marmanillo Javier-->
-<!-- </div>-->
-<!--            <div>-->
-
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
+    <hr class="slash-1">
     <p>©Copyright 2050 de nadie. Todos los derechos revertidos.</p>
+    
 </footer>
 <style>
     .pie_pagina{
-        color: #fff;
-        /*background-color: rgba(0, 0, 0, 0.5);*/
-        background-color: rgba(103, 25, 25, 0.5);
-        bottom: 0;
-        height: 50px;
-        left: 0;
-        position: absolute;
-        width: 100%;
-        max-width:150%;
-        /*width: 1265px;!* se cambia de 100% a:
-        max-width:2500px; se cambia de 150% a:
-        min-width: 300px; se cambia de 100% a:*/
-        z-index: 20;
 
+        height: 50;
+        position: relative;
+margin-top: -63px; /* ponga en negativo el alto de su pie de página para nuestro    ejemplo usamos 63, usted debe remplazarlo según su diseño   */
+height: 63px;
+clear:both;
+width:100%;
     }
-</style>
+
+    .slash-1 {
+  height: 30px;
+  color: rebeccapurple;
+  color: color;
+  background-image: linear-gradient(-45deg,
+    transparent,
+    transparent 25%,
+    currentColor 25%,
+    currentColor 50%,
+    transparent 50%,
+    transparent 75%,
+    currentColor 75%);
+  background-size: 10px 10px;
+  width: 1200px;
+}
+
+.slash-2 {
+  height: 30px;
+  color: darkmagenta;
+  color: color;
+  background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="3px" height="3px" viewBox="0 0 3 3" fill="#{$color}"><polygon points="0,0.5 0,1.5 1.5,3 2.5,3"/><polygon points="2.5,0 1.5,0 3,1.5 3,0.5"/></svg>');
+  background-size: 10px 10px;
+  width: 100%;
+}
+
